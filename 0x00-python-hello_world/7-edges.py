482fc9f726a21572cfa1c592cf52e375dcdc5ac6
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-09 13:38:53 +0000

    Update

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
-f3l = word[:2]
-f2l = word[:1]
+f3l = word[:3]
+f2l = word[-2:]
 mid = word[1:8]
 print(f"First 3 letters: {f3l}")
 print(f"Last 2 letters: {f2l}")
