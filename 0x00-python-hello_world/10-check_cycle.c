fb29bf1b19a4b6c4e44f71581e83f761adcd5ea7
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-09 17:26:49 +0000

    efficient

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,28 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if there is a cycle
- * @lists: linked list to check
- *
- * Return 1 || 0
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+    listint_t *tortoise, *hare;
 
-	if (!list)
-		return (0);
+    if (list == NULL || list->next == NULL)
+        return (0);
 
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (1);
-	}
-	return (0);
+    tortoise = list;
+    hare = list->next;
+
+    while (tortoise != hare)
+    {
+        if (hare == NULL || hare->next == NULL)
+            return (0);
+
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+    }
+
+    return (1);
 }
