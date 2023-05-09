50fc7eeebbbc276e861e56e9cb9cfe55c6cf96a7
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-09 14:30:38 +0000

    slice

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 f3l = word[:3]
 f2l = word[-2:]
-mid = word[1:8]
+mid = word[1:-1]
 print(f"First 3 letters: {f3l}")
 print(f"Last 2 letters: {f2l}")
 print(f"Middle word: {mid}")
