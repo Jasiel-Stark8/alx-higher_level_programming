52e505bcdb91226b25783a86a02d8ce91a592c72
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-11 21:46:55 +0000

    pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 nums = []
 for i in range(0, 10):
-    for j in range(0, 10):
-        if i != j and not (f"{i}{j}" in nums or f"{j}{i}" in nums):
-            nums.append(f"{i}{j}")
-            print(f"{i}{j}", end=", " if not (i == 9 and j == 9) else "\n")
+    for j in range(i + 1, 10):
+        if i != j:
+            if i == 8 and j == 9:
+                print("{:d}{:d}".format(i, j))
+            else:
+                print("{:d}{:d}".format(i, j), end=", ")
