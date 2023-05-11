1e8997dc68c87eb71b852c9c666cd4f0242d4c43
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-11 19:12:00 +0000

    up and up and up and up and up and up and up

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     up = ""
-    for i in str.split():
-        if 97 <= ord(c) <= 122:
-            up += chr(ord(str) - 32)
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            up += chr(ord(i) - 32)
         else:
-            result += c  # Leave it as it is
-    print(result)
+            up += i  # Leave it as it is
+    print(up)
+
+# Test the function
+# uppercase("hello world")
