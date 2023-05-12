a525b97a4580514f54224ab98044337dabe9dff1
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 16:31:42 +0000

    fix string not printing when n is not specified

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,6 @@ def remove_char_at(str, n):
     for i in str:
         if i == str[n]:
             return str[:n] + str[n + 1:]
-
+        else:
+            return str
 # print(remove_char_at('alphabet', 5))
