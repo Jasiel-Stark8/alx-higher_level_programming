4a8705d2aa0fdddfd1e0934c23c797ffd53e2b86
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 17:12:21 +0000

    magic

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+# import dis
 def magic_calculation(a, b, c):
-    return a + b + c
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return a * b - c
+
 
 # print(magic_calculation(1, 2, 3))
+# dis.dis(magic_calculation)
