e0f0b12a35167c62dd996384cce749e415660d3e
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 01:50:43 +0000

    format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     num = len(sys.argv) - 1
     # subtract 1 because the script name is also included in sys.argv
     if num == 1:
-        print(f"{num} argument:")
+        print("{} argument:".format(num))
     else:
-        print(f"{num} arguments:")
+        print("{} argument:".format(num))
 
     for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
+        print("{}: {}".format(i, arg))
