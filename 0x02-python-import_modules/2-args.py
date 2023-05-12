fbe0ab44195858d55f9b8741ec95041c96a3d076
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 01:53:10 +0000

    multiple agrgs fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     if num == 1:
         print("{} argument:".format(num))
     else:
-        print("{} argument:".format(num))
+        print("{} arguments:".format(num))
 
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
