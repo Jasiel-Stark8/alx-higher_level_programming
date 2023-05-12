daf1f7567b2cf0be906ab4a4cd9f85c678c90230
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 16:50:38 +0000

    if line

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        if i == str[n]:
-            return str[:n] + str[n + 1:]
-        else:
-            return str
+    if n >= 0 and n < len(str):
+        return str[:n] + str[n + 1:]
+    else:
+        return str
+
 print(remove_char_at('chicago', 15))
