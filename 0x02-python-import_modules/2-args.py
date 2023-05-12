cb862cc2f1c58ea6bf3fed82d3f36220c4f2d732
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 22:01:31 +0000

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(arg_count))
 
-    for i in range(1, len(sys.argv)):
+    for i in range(arg_count):
         print("{}: {}".format(i, sys.argv[i]))
