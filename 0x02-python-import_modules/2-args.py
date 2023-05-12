0c374e51b1599b0cd225e265aae4be83255b275f
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 01:57:57 +0000

    call sys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    num = len(sys.argv) - 1
-    # subtract 1 because the script name is also included in sys.argv
 
-    if num == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif num == 1:
-        print("{} argument:".format(num))
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(num))
-
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
