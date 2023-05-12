e39797785dd27645ef556ef1c899e41f5192269f
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-12 16:37:02 +0000

    fix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     for i in str:
-        if i == n:
+        if i == str[n]:
             return str[:n] + str[n + 1:]
         else:
             return str
-print(remove_char_at('chicago', 3))
+print(remove_char_at('chicago', 15))
