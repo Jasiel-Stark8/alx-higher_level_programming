517750c2f38cc2849c8421a8f9f84e876ef98f93
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 21:56:14 +0000

    comment

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,3 +5,11 @@ def multiple_returns(sentence):
     else:
         return len(sentence), sentence[0]
     # return len(sentence), None
+
+# def multiple_returns(sentence):
+#     if not sentence:
+#         return len(sentence), None
+#     for i in sentence:
+#         if isinstance(i, str):
+#             return len(sentence), i
+#     # return len(sentence), None
