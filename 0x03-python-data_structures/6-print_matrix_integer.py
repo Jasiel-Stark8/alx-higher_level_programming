812c0f1f3f936208f51a12435625581989c0c2a9
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 19:09:06 +0000

    Escape the Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in []:
         for j in []:
             for k in []:
-                if i == 8 and j == 9:
-                    print("{:d}{:d}".format(i, j))
-                else:
-                    print("{:d}{:d}".format(i, j), end=", ")
+                print("{:d}{:d}{:d}".format(i, j, k), end=", ")
