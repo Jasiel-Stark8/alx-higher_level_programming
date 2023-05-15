bfafb422f7bb7e212d3af20a74323bcb8aa95451
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 19:07:06 +0000

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in []:
+        for j in []:
+            for k in []:
+                if i == 8 and j == 9:
+                    print("{:d}{:d}".format(i, j))
+                else:
+                    print("{:d}{:d}".format(i, j), end=", ")
