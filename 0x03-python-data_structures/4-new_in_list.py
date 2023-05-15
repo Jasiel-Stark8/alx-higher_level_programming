5070f49b6b07bfa514edf3b6e7331b60b01627d3
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 17:18:00 +0000

    short idx

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,4 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         print(my_list)
     else:
-        my_list.insert(idx, element)
-        print(my_list)
+        my_list[idx] = element
