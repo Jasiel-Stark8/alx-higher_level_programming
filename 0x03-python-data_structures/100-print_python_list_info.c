1ff224f722cee2f08b35981a4ce4bafb221da334
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 23:06:03 +0000

    object CPython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,18 @@
 #include <Python.h>
 
-void print_python_list_info(PyObject *p){
-    if(p == NULL)
-        return;
-    if(PyList_Check(p))
-        printf("List\n");
-    else if(PyTuple_Check(p))
-        printf("Tuple\n");
-    else if(PyDict_Check(p))
-        printf("Dict\n");
-    else if(PySet_Check(p))
-        printf("Set\n");
-    else if(PyFrozenSet_Check(p))
-        printf("FrozenSet\n");
-    else if(PyLong_Check(p))
-        printf("Integer\n");
-    else if(PyFloat_Check(p))
-        printf("Float\n");
-    else if(PyUnicode_Check(p))
-        printf("String\n");
+void print_python_list_info(PyObject *p)
+{
+    long int size = PyList_Size(p);
+    long int alloc = ((PyListObject *)p)->allocated;
+    PyObject *item;
+    Py_ssize_t i;
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", alloc);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
 }
