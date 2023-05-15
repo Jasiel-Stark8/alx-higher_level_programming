10a6a057977af183d8a99c851afc1f73ecdab562
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 17:49:45 +0000

    a listttt

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    a = my_list[:]
+    
     if idx < 0 or idx >= len(my_list):
-        print(my_list)
-    else:
-        my_list[idx] = element
+        a[idx] = element
+    
+    return a
