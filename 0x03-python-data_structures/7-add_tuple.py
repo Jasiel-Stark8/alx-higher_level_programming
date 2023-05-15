a598d1cd70fb24fc803dd776e9fb64ff3d818865
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 19:55:36 +0000

    index add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = tuple_a + tuple_b
+    tuple_c = tuple_a() and tuple_b()
+    for i in tuple_a:
+        for j in tuple_b:
+            print(i[0] + j[0] and i[1] + j[1])
     return tuple_c
-
-# print(add_tuple((1, 2, 3), (4, 5, 6)))
