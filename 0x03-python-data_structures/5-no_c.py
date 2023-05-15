a1e3093e4e6714ab5f2146bbd8f0a8a604349d44
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 18:49:27 +0000

    join

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
+#!/usr/bin/python3
 def no_c(my_string):
-    # Create a new string without 'c' or 'C'
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
+    chars = [char for char in my_string if char != 'c' and char != 'C']
+    new_string = ''.join(chars)
 
     return new_string
