edb5457a8a9ac58af2a76d899fa80450de5ee749
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 13:24:44 +0000

    just keep printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    print(my_list.reverse())
+    my_list.reverse()
+    print(my_list)
     # print(my_list[::-1])
-
-# if __name__ == '__main__':
-#     my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#     print_reversed_list_integer(my_list)
