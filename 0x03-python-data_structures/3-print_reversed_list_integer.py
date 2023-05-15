c8ce342d8eca607618504186204c68157856c242
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 14:53:27 +0000

    lets see 10

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+# def print_reversed_list_integer(my_list=[]):
+#     for i in my_list[::-1]:
+#         print("{:d}".format(i))
+
+
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list[::-1]:
+    my_list.reverse()
+    for i in my_list:
         print("{:d}".format(i))
