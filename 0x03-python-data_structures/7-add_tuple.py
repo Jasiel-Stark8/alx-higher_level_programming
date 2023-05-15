feceadeead08c2ded535dd87ccb76f421727a70a
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 19:44:56 +0000

    simple is sometimes the best

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = tuple(map(lambda x: x, tuple_a + tuple_b))
+    tuple_c = tuple_a + tuple_b
     return tuple_c
 
 # print(add_tuple((1, 2, 3), (4, 5, 6)))
