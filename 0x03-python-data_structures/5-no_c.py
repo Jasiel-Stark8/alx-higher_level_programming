00f79a70bf2365454597d93b2476c2165538cdfc
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 18:35:31 +0000

    n

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string.split():
-        if i is 'c' and i is 'C':
-            return i.strip()
-        print(my_string)
+    string = ""
+    for char in my_string.split():
+        if char != 'c' and char != 'C':
+            string += char
+        print(string)
