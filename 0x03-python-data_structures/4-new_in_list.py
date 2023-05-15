40d985e6b94c9edb04d4890ea8379c714ee9c400
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-15 17:53:20 +0000

    haha

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    a = my_list
-
-    if idx >= 0 or idx < len(my_list):
-        a[idx] = element
-
-    return a
+    # Create a new copy of the list
+    new_list = my_list[:]
+    
+    # Check if idx is a valid index
+    if idx >= 0 and idx < len(my_list):
+        # Replace the element at idx
+        new_list[idx] = element
+    
+    # Return the new list
+    return new_list
