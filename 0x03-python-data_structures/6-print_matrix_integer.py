849c9521b91d1ead6c969d1c40a7475705f34d58
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-16 00:16:21 +0000

    c

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+# def print_matrix_integer(matrix=[[]]):
+#     for row in matrix:
+#         for i in range(len(row)):
+#             if i == len(row) - 1:
+#                 print("{:d}".format(row[i]))
+#             else:
+#                 print("{:d}".format(row[i]), end=" ")
+
+#!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]))
-            else:
-                print("{:d}".format(row[i]), end=" ")
+        for column in row:
+            print("{:d}".format(column), end=" " if column != row[-1] else "")
         print()
