2c89b1cd6214bc5b779168e824df0e6936e4c660
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-17 23:45:39 +0000

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,9 +4,3 @@ def search_replace(my_list, search, replace):
         if search in my_list[i]:
             my_list[i] = my_list[i].replace(search, replace)
     return my_list
-
-if __name__ == "__main__":
-    my_list = ["hello", "world", "this", "is", "a", "list"]
-    search = "is"
-    replace = "a"
-    print(search_replace(my_list, search, replace))
