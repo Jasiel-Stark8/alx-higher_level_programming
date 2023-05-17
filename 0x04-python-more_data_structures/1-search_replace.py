4be7045cb78cc13957a41a7c8f50284e744d873c
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-17 23:34:21 +0000

    RSA

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for element in my_list:
-        search[element] = replace
-        return my_list
+    for i in range(len(my_list)):
+        if search in my_list[i]:
+            my_list[i] = my_list[i].replace(search, replace)
+    return my_list
+
+if __name__ == "__main__":
+    my_list = ["hello", "world", "this", "is", "a", "list"]
+    search = "is"
+    replace = "a"
+    print(search_replace(my_list, search, replace))
