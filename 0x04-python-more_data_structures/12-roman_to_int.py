1d1247ae276ec0c46358e2a431169ff1a4c3fde8
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 16:50:26 +0000

    Rome.exe

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-def roman_to_int(self, roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-    roman = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    number = 0
-    for i in range(len(roman_string)):
-        if i < len(roman_string) - 1:
-            if roman[roman_string[i]] < roman[roman_string[i + 1]]:
-                number -= roman[roman_string[i]]
-            else:
-                number += roman[roman_string[i]]
-    number += roman[roman_string[-1]]  # Add the last roman numeral
-    return number
+class Solution:
+    def roman_to_int(self, roman_string):
+        if not isinstance(roman_string, str) or roman_string is None:
+            return 0
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        number = 0
+        for i in range(len(roman_string)):
+            if i < len(roman_string) - 1:
+                if roman[roman_string[i]] < roman[roman_string[i + 1]]:
+                    number -= roman[roman_string[i]]
+                else:
+                    number += roman[roman_string[i]]
+        number += roman[roman_string[-1]]  # Add the last roman numeral
+        return number
 
+
+# task = Solution()
 # print(task.roman_to_int('MCMXCIV'))  # Output: 1994
