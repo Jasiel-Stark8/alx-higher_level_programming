7380ace424f30ea9257a6f946728b5861dddc3f8
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 17:24:40 +0000

    utility module

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,4 +3,4 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
     for i in my_list:
-        return sum(my_list[i]) / len(my_list)
+        return sum(my_list) / len(my_list)
