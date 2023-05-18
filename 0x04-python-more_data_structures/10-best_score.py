a717dd12d27f3850fcafb9ef31f7b387b96f5ffe
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 14:31:40 +0000

    tests and cache

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:  # Return None if dict is empty
-        return None
-    return max(a_dictionary, key=a_dictionary.get())
-# print(best_score({1: 2, 2: 4, 3: 6}))
+    score = 0
+    for key in a_dictionary:
+        if a_dictionary[key] > score:
+            score = a_dictionary[key]
+    return score
