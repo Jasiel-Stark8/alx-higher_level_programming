69385015460a3c32dda983b896aec1d56cd9a95a
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 14:33:33 +0000

    safe guard

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     score = 0
+    if score not in a_dictionary:
+        return None
     for key in a_dictionary:
         if a_dictionary[key] > score:
             score = a_dictionary[key]
