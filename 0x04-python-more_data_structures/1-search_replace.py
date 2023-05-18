b009260cac8c945949e3b285a3fb172dcb34a069
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 00:32:31 +0000

    li

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     if replace < 0 or replace >= len(my_list):
         return my_list
-    
-    for i in range(len(my_list)):
+
+    for i in my_list:
         if search in my_list[i]:
             my_list[i] = my_list[i].replace(search, replace)
     return my_list
