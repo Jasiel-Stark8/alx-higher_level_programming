de64a31cdbbb80c63a457fa93bc90df553e66735
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 11:00:44 +0000

    God Answers Prayer

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,5 @@ def uniq_add(my_list=[]):
     for i in my_list:
         result += i
     return result
-
-
-if __name__ == "__main__":
-    print(uniq_add([1, 2, 1, 4, 3, 3, 5, 5, 1, 7, 8]))
+# if __name__ == "__main__":
+#     print(uniq_add([1, 2, 1, 4, 3, 3, 5, 5, 1, 7, 8]))
