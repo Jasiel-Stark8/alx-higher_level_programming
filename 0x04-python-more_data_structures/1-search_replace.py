3296e63111b823ebb3675927e0c0b5e3a32eb977
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 00:29:41 +0000

    conditional

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if replace < 0 or replace >= len(my_list):
+        return my_list
+    
     for i in range(len(my_list)):
         if search in my_list[i]:
             my_list[i] = my_list[i].replace(search, replace)
