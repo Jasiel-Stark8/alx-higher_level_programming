be8273f3bcd78570ac51b7e2ca19978a3a3afa2e
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-18 14:54:32 +0000

    new list#

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.keys() * number
-    return map(new_list * number in my_list)
+    new_list = map(my_list.keys() * number)
+    return new_list
