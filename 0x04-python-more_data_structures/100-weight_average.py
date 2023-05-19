9c03a07e41c46e5196956aa463550667666af165
Jasiel-Stark8 <Jazielair777@gmail.com>
2023-05-19 23:13:57 +0000

    weight

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,5 +3,5 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
 
-    avg = sum(my_list) / len(my_list)
+    avg = sum(x[0] * x[1] for x in my_list) / sum(x[1] for x in my_list)
     return avg
