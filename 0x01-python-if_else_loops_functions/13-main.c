fe2dbcf6fbb99097f98c1aceb6aacfe558b2de84
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-25 21:18:27 +0000

    mik

---
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+    int arr[10];
+    printf("ENter your numbers: ");
+    scanf("%d", &arr[10]);
+
+    for (i = 0; i < arr; i++)
+    {
+        // sum = 0;
+        // sum += arr[i];
+        // return sum;
+        printf("%d\n", arr[i]);
+    }
+}
