d71d9aa8f312580202a46de505656da072352cba
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-29 22:53:21 +0000

    Raise IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
                 break
         print()
         return count
-    except:
+    except IndexError:
         print("Error while printing list.")
     return count
