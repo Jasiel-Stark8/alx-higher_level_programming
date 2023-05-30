8aea1cc4169f2e97c3f91bfc3f5f12cb22e39296
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-30 17:25:29 +0000

    magic result exception

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(0, 103):
+        try:
+            if i < a:
+                result += a
+        except IndexError:
+            result += b[i]
+        result = a + b
+    return result
