234a6ad4a6a289c08a540b23aac5d50eebd5a74d
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-30 14:37:09 +0000

    He Raised Me Up!!!

--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -3,5 +3,5 @@ raise_exception = __import__('5-raise_exception').raise_exception
 
 try:
     raise_exception()
-except TypeError:
-    print("Exception has been raised")
+except TypeError as te:
+    print("Exception raised")
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,3 +1,3 @@
 #!/usr//bin/python3
 def raise_exception():
-    raise Exception("Error has been raised!")
+    raise TypeError("Error has been raised!")
