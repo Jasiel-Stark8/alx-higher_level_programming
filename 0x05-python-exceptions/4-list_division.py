09f23243e12cc04634cb87835aaf335431b9e498
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-30 14:10:15 +0000

    tey exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,17 +3,26 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     try:
-        for i in range(len(my_list_1)):
-            for j in range(len(my_list_2)):
-                if my_list_2[j] == 0:
-                    raise ZeroDivisionError("division by 0")
-                elif my_list_1[i] % my_list_2[j] == 0:
-                    new_list.append(0)
-                else:
-                    new_list.append(my_list_1[i] / my_list_2[j])
-    except ZeroDivisionError:
-        print("division by 0")
-    except (TypeError, IndexError):
-        new_list = None
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                new_list.append(result)
+            except ZeroDivisionError:
+                new_list.append(0)
+                print("division by 0")
+            except (TypeError):
+                new_list.append(0)
+                print("wrong type")
+            except IndexError:
+                new_list.append(0)
+                print("out of range")
     finally:
         print(new_list)
+
+
+# Test
+list_1 = [10, 20, 30, 40]
+list_2 = [2, 0, 5]
+length = 5
+result = list_division(list_1, list_2, length)
+print(result)
