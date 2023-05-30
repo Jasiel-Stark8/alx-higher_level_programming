d37051d4c911401ed01d1a5befef4fcc59ee49a0
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-30 13:37:50 +0000

    append

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,10 +4,12 @@ def list_division(my_list_1, my_list_2, list_length):
     try:
         for i in range(len(my_list_1)):
             for j in range(len(my_list_2)):
-                if i % j == 0:
-                    return 0
+                if my_list_2[j] == 0:
+                    raise ZeroDivisionError("division by 0")
+                elif my_list_1[i] % my_list[j] == 0:
+                    new_list.append(0)
                 else:
-                    new_list = my_list_1[i] / my_list_2[j]
+                    new_list.append(my_list_1[i] / my_list_2[j])
         else:
             if len(my_list_1) < len(my_list_2):
                 print("out of range")
