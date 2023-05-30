a5c347a9d32c39257ef39a4d858a81da23a4647e
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-30 17:40:01 +0000

    Final exception return

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(0, 103):
+    for i in range(1, 4):
         try:
-            if i < a:
-                result += i
+            if i > a:
+                raise Exception("Too far")
+            result += a ** b / i
             else:
                 result += b[i]
         except IndexError:
             result = a + b
             break
+    result += a + b
     return result
