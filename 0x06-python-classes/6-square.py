ad66bc2dfa7296bf14cb24777b10b7ad2ec592cb
Jasiel-Stark8 <jazielair777@gmail.com>
2023-05-31 12:29:50 +0000

    validate print position method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,9 +38,13 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
+        elif self.__position[1] > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end="")
                 print("")
-
