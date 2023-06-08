72a70e2882094c53081b55fdacbdef3a919aa714
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-08 11:21:48 +0000

    hmmm

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """Class defining Rectangle"""
 
     number_of_instances = 0
-    print_symbol = "#"
+    # print_symbol = "#"
 
     def __init__(self, width=0, height=0, print_symbol=''):
         self.width = width
