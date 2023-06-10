9f1a89b22676508b2ff12c7a95ba058bcd32adf6
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-10 22:25:00 +0000

    test_driven_development_test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,8 +6,8 @@ def add_integer(a, b=98):
     """This module adds 2 integers and returns the sum \
         which is casted in an int()"""
 
-    if a is not isinstance(a, int):
-        raise TypeError("a must be an integer")
-    if b is not isinstance(b, int):
-        raise TypeError("b must be an integer")
+    if a is not isinstance(a, int) and a is not isinstance(a, float):
+        raise TypeError("a must be an integer or a float")
+    if b is not isinstance(b, int) and a is not isinstance(a, float):
+        raise TypeError("b must be an integer or float")
     return int(a) + int(b)
