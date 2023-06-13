41a78b5ef8a878cf047a43e5dac13ce01b1be9d8
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 15:01:20 +0000

    validate width and height through inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""A module that defines a class Rectangle that inherits from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle"""
+
+    def __init__(self, width, height):
+        """
+        Initializes a new instance of the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
