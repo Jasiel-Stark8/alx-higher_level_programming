5b881dc21aab758a6ef2b673f94fd73b87911919
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 12:02:04 +0000

    exat instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
-    """validate if object is an exact instance of specifeid class"""
-    if isinstance(obj, a_class):
+    """validate if object is an exact instance of specifeid class
+
+    Args: (obj) is the object to be validated
+    is matched against an exact class name
+
+    Returns:
+    True if obj is an exact instance of a_class
+    False otherwise
+    """
+
+    if type(obj) == a_class:
         return True
-    else:
-        return False
+    return False
