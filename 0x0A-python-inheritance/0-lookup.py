9358c8f2efd4c13cd806c38fcde77b4a281c1cc9
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 10:46:29 +0000

    reduce docstring length

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""""Defining a function that returns the list of available attributes and methods of an object"""
+""""Defining a function that returns the list of available attributes \
+    and methods of an object"""
 
 
 def lookup(obj):
