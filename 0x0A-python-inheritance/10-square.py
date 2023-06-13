ec8fa024824b804ab15c7537f2c59e1d80968ea4
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 15:48:09 +0000

    implements suare through inheritace from Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""A module that defines a class Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent Square class"""
+
+    def __init__(self, size):
+        """Initialize a new Square"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return square area"""
+        return self.__size * self.__size
