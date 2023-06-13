48061ff2a3802558d6a5760c60a441326c4f6edf
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 12:49:47 +0000

    RULE: wrap issubclass first argument 'obj' in type

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,6 +7,6 @@
 
 def is_kind_of_class(obj, a_class):
     """Check is obj is an instance of a class or a subclass"""
-    if isinstance(obj, a_class) or issubclass(obj, a_class):
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
         return True
     return False
