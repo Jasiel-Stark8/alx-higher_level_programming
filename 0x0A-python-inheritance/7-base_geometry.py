a7bcb4f9bf1a758d20c9dea6c02abe7c6317a9b3
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 14:42:17 +0000

    format string and add docstring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,14 +3,15 @@
 
 
 class BaseGeometry:
-    """ Initialize BaseGeometry"""
+    """BaseGeometry class"""
 
     def area(self):
         """Raise an exception if the area is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates value as an integer"""
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
