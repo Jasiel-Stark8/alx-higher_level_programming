2470ee4bf8791aac2c02c54c028f135020f2b473
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 12:43:43 +0000

    check isinstance of a subclass

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    Define a function that checks if an object is either and instance of a local \
+    class or an inherited class
+    """
+
+
+def is_kind_of_class(obj, a_class):
+    """Check is obj is an instance of a class or a subclass"""
+    if isinstance(obj, a_class) or issubclass(obj, a_class):
+        return True
+    return False
