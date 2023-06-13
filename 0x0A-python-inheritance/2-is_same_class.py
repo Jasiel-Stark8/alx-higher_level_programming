dcd0a88ba27a4b53eda4a25b43f4c5ea9cb1d380
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 11:52:40 +0000

    class obj validation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """validate if object is an exact instance of specifeid class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
