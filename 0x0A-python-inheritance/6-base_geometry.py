5d1c952c7f24e6a48cc13419e8c3e734da56ac44
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-13 14:00:43 +0000

    raise exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Defnie an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ Initialize BaseGeometry"""
+
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
