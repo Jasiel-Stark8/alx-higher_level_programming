bef654f1791994ee7deead80f2ab0fd78c4fc5fa
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-14 23:39:44 +0000

    GO BACK

---
+++ 0x0B-python-input_output/tests1/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a module that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write a string to a text file (UTF8) and return the number of
+    characters written.
+    """
+    try:
+        with open(filename, 'w', encoding="UTF-8") as file:
+            file.write(text)
+            return len(text)
+    except OSError:
+        return -1  # Returns -1 when an error occurres
