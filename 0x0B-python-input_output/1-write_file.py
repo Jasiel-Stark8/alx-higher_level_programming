f584d4a042901a1c511c812b298a36f3b41965e3
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-14 23:31:44 +0000

    remove comment

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,4 +12,4 @@ def write_file(filename="", text=""):
             file.write(text)
             return len(text)
     except OSError:
-        return -1  # Returns -1 when an error occurres
+        return -1
