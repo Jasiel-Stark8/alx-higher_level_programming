9eb2c365e3d566bdcdb28e8635bb4803d62df363
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-14 22:54:42 +0000

    write to fileUTF8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Ddfine a module that writes to a file"""
+"""Define a module that writes to a file"""
 
 
 def write_file(filename="", text=""):
@@ -7,6 +7,7 @@ def write_file(filename="", text=""):
     Write a string to a text file (UTF8)
     Returns: Number of characters writen
     """
-    if filename is None:
-        with open(filename, 'w', encoding="UTF8", text=text) as txt_len:
-            return len(txt_len)
+    if filename == "":
+        with open(filename, 'w', encoding="UTF8") as txt:
+            txt.write(text)
+        return len(text)
