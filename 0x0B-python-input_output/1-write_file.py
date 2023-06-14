3597ab0aad25f1a0f3db2806c2e789287b130e2f
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-14 23:35:49 +0000

    utf

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     characters written.
     """
     try:
-        with open(filename, 'w', encoding="UTF-8") as file:
+        with open(filename, 'w', encoding="utf-8") as file:
             file.write(text)
             return len(text)
     except OSError:
