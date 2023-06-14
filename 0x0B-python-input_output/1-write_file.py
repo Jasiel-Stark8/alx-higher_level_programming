750472db515127db6907729ff5a8d5cae67fb9ad
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-14 23:18:45 +0000

    add except OS block

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,7 +7,11 @@ def write_file(filename="", text=""):
     Write a string to a text file (UTF8)
     Returns: Number of characters written
     """
-    if filename == "":
-        with open(filename, 'w', encoding="UTF8") as file:
-            file.write(text)
-        return len(text)
+    try:
+        if filename == "":
+            with open(filename, 'w', encoding="UTF8") as file:
+                file.write(text)
+            return len(text)
+    except OSError:
+        return -1  # Returns -1 to indicate error an error occured 
+# during file writing
