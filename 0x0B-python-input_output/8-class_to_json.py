81bb1b9beb75156ce4ca89040dc6fa943a42eda9
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-15 18:40:01 +0000

    return dictionary of objects attributes

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,13 +8,17 @@ import json
 
 def class_to_json(obj):
     """
-    Returns dictionary description of python Data structures
-    (list, dictionary, string, integer and boolean)
+    Returns a dictionary description of an object's attributes
+    with simple data structures suitable for JSON serialization.
     """
+    if isinstance(obj, (list, dict, str, int, bool)):
+        return obj
+
     result = {}
     for attr in obj.__dict__:
         value = obj.__dict__[attr]
-        if isinstance(obj, (list, dict, str, int, bool)):
+        if isinstance(value, (list, dict, str, int, bool)):
             result[attr] = value
 
     return result
+
