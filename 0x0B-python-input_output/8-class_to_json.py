6574d6dc0b3bec894962a62c1ecedb4347f7d145
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-15 18:13:06 +0000

    return python data structure as Json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""A module that returns the dictionary description with simple
+data structure (list, dictionary, string, integer and boolean) 
+for JSON serialization of an object:
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns dictionary description of python Data structures 
+    (list, dictionary, string, integer and boolean)
+    """
+    return obj.__dict__
