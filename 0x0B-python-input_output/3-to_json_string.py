56c57794c4cb738f976222e40d0bec0ed4acf9b9
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-15 11:38:45 +0000

    load json object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a modile that returns the JSON representation of an object(string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Convert object(string) to JSON"""
+    return json.load(my_obj)
