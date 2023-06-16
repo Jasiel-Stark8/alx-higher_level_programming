44e5b26e5377958ef522356170d33f691c642333
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-16 12:31:47 +0000

    init file

---
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""0-main.py"""
+from models.base import Base
+
+if __name__ == "__main__":
+    my_base = Base()
+    print(my_base.id)
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a parent class Base"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes a Base object"""
+        self.id = id
+        if self.id is not None:
+            self.id = id
+        __nb_objects += id
