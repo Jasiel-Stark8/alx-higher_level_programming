5a19126cc8e07e36aa89d8f9cb90040815c15b8f
Jasiel-Stark8 <jazielair777@gmail.com>
2023-06-20 10:08:04 +0000

    Rectangle update

--- 0x0C-python-almost_a_circle/models/retangle.py
+++ 0x0C-python-almost_a_circle/models/retangle.py
@@ -1,122 +1,97 @@
 #!/usr/bin/python3
-"""Define a parent class Rectangle"""
+"""Module for Rectangle class"""
 from models.base import Base
-Base = __import__('').Base
 
 
 class Rectangle(Base):
-    """Class that inherits from Base class and represents a Rectangle"""
+    """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize Rectangle"""
+        """Class constructor"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        """Get width"""
+        """Getter for width"""
         return self.__width
 
     @width.setter
-    def width(self, width):
-        """Validate and set width"""
-        if not isinstance(width, int):
+    def width(self, value):
+        """Setter for width"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if width <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
-        """Get height"""
+        """Getter for height"""
         return self.__height
 
     @height.setter
-    def height(self, height):
-        """Validate and set height"""
-        if not isinstance(height, int):
+    def height(self, value):
+        """Setter for height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if height <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
-        """Get x-coordinate"""
+        """Getter for x"""
         return self.__x
 
     @x.setter
-    def x(self, x):
-        """Validate and set x-coordinate"""
-        if not isinstance(x, int):
+    def x(self, value):
+        """Setter for x"""
+        if type(value) is not int:
             raise TypeError("x must be an integer")
-        if x < 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = x
+        self.__x = value
 
     @property
     def y(self):
-        """Get y-coordinate"""
+        """Getter for y"""
         return self.__y
 
     @y.setter
-    def y(self, y):
-        """Validate and set y-coordinate"""
-        if not isinstance(y, int):
+    def y(self, value):
+        """Setter for y"""
+        if type(value) is not int:
             raise TypeError("y must be an integer")
-        if y < 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = y
+        self.__y = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
 
     def display(self):
-        """Print Rectangle with the '#' character"""
+        """Prints the rectangle with the character '#'"""
         print("\n" * self.y, end="")
-        for i in range(self.height):
-            print(" " * self.x, end="")
-            print("#" * self.width)
+        print((" " * self.x + "#" * self.width + "\n") * self.height, end="")
 
     def __str__(self):
-        """Print string representation of Rectangle"""
-        return f"[Reectangle] ({self.id}) {self.x}/{self.y} \
-            - {self.width}/{self.height}"
+        """String representation of the rectangle"""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
+            {self.width}/{self.height}"
 
-    def update(self, *args):
-        """Update Rectangle"""
-        # handle if none is given
-        if len(args) <= 1:
-            raise ValueError("Coordinates can't be empty")
-
-        # if only x is given
-        if len(args) == 2:
-            if not isinstance(args[1], int):
-                raise TypeError("First argument must be an \
-                    integer representing 'x'")
-            if args[1] < 0:
-                raise ValueError("'x' must be >= 0")
-            self.__x = args[1]
-            return
-
-        # if x and y are given
-        if len(args) == 3:
-            if not isinstance(args[1], int):
-                raise TypeError("First argument must be an \
-                    integer representing 'x'")
-            if args[1] < 0:
-                raise ValueError("'x' must be >= 0")
-            self.__x = args[1]
-
-            if not isinstance(args[2], int):
-                raise TypeError("Second argument must be an \
-                    integer representing 'y'")
-            if args[2] < 0:
-                raise ValueError("'y' must be >= 0")
-            self.__y = args[2]
-            return
-
-        # handle when more than 3 arguments (filename inclusive) are given
-        if len(args) > 3:
-            raise ValueError("Too many arguments provided, \
-                maximum of 2 (x, y) are allowed")
-        
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        attributes = ["id", "width", "height", "x", "y"]
+        if args:
+            for i, value in enumerate(args):
+                if i < len(attributes):
+                    setattr(self, attributes[i], value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key in attributes:
+                    setattr(self, key, value)
